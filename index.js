b857c0b8d57553ba01626efbc8c428a2c2cd4eeb
Emma Freeman <freemoe04@gmail.com>
2017-10-04 20:24:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function happyHolidays(){
   return "Happy holidays!";
 }
 function happyHolidaysTo(name) {
-  return 'Happy holidays, Emma!';
+  return 'Happy holidays, you!';
 };
 function happyHolidayTo(holiday,name){
   return 'Happy ${holiday}, ${name}!';
